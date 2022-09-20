5d4ab4e6dd7dc4dcbdd01e7ed9e8758d249b6cf0
Claudiney Silva <84917784+Csld72k@users.noreply.github.com>
2022-09-20 10:12:23 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,49 +1,123 @@
-import { Elements } from "./elements.js";
-import Controls from './controls.js';
-import Timer from './timer.js';
-import Events from "./events.js";
-import Sound from "./sound.js";
-
-const {
-  secondsDisplay,
-  minutesDisplay,
-  checkboxForest,
-  checkboxRain,
-  checkboxCoffeeShop,
-  checkboxFireplace,
-  buttonPlay,
-  buttonPause,
-  buttonReduce,
-  buttonLightMode,
-  buttonDarkMode
-} = Elements;
-
-const controls = Controls({
-  buttonPlay,
-  buttonPause,
-  buttonReduce,
-  buttonLightMode,
-  buttonDarkMode
-});
-
-const sound = Sound({
+export default function Sound({
   checkboxForest,
   checkboxRain,
   checkboxCoffeeShop,
   checkboxFireplace,
   buttonPlay
-});
+}) {
 
-const timer = Timer({
-  secondsDisplay,
-  minutesDisplay,
-  hidePauseAndShowPlay: controls.hidePauseAndShowPlay,
-  disableAndEnableButtonReduce: controls.disableAndEnableButtonReduce,
-  playFinishTimerSound: sound.finishSound,
-  pauseAllSounds: sound.pauseAllSounds
-});
+  const forestSound = new Audio('https://github.com/Csld72k/claudiney/blob/master/cursos/rocketseat/programa-explorer/stage-05/04-focus-timer-2/sounds/Floresta.wav?raw=true');
+  const rainSound = new Audio('https://drive.google.com/u/0/uc?id=1Ip8xBqAUJ-bty51Wz8JBtX_bWXCgA0P2&export=download');
+  const coffeeShopSound = new Audio('https://github.com/Csld72k/claudiney/blob/master/cursos/rocketseat/programa-explorer/stage-05/04-focus-timer-2/sounds/Cafeteria.wav?raw=true');
+  const fireplaceSound = new Audio('https://github.com/Csld72k/claudiney/blob/master/cursos/rocketseat/programa-explorer/stage-05/04-focus-timer-2/sounds/Lareira.wav?raw=true');
+  const finishTimerSound = new Audio('https://github.com/Csld72k/claudiney/blob/master/cursos/rocketseat/programa-explorer/stage-05/04-focus-timer-2/sounds/audios_kichen-timer.mp3?raw=true');
 
-// START WITH buttonReduce DISABLED BEEN ENABLED WHEN MINUTES >= 5
-controls.disableAndEnableButtonReduce('00');
+  forestSound.loop = true;
+  rainSound.loop = true;
+  coffeeShopSound.loop = true;
+  fireplaceSound.loop = true;
+  forestSound.volume = .5;
+  rainSound.volume = .5;
+  coffeeShopSound.volume = .5;
+  fireplaceSound.volume = .5;
 
-Events({ controls, timer, sound });
+  function onOffForestSound(elementId) {
+
+    let inputCheckbox = document.querySelector(elementId);
+
+    inputCheckbox.checked === true ? forestSound.play() : forestSound.pause();
+
+  }
+
+  function onOffRainSound(elementId) {
+
+    let inputCheckbox = document.querySelector(elementId);
+
+    inputCheckbox.checked === true ? rainSound.play() : rainSound.pause();
+
+  }
+
+  function onOffCoffeeShopSound(elementId) {
+
+    let inputCheckbox = document.querySelector(elementId);
+
+    inputCheckbox.checked === true ? coffeeShopSound.play() : coffeeShopSound.pause();
+
+  }
+
+  function onOffFireplaceSound(elementId) {
+
+    let inputCheckbox = document.querySelector(elementId);
+
+    inputCheckbox.checked === true ? fireplaceSound.play() : fireplaceSound.pause();
+
+  }
+
+  function finishSound() {
+
+    if (buttonPlay.classList.contains('hide')) {
+
+      finishTimerSound.volume = .1;
+      finishTimerSound.play();
+
+    }
+
+  }
+
+  function pauseAllSounds() {
+
+    if (buttonPlay.classList.contains('hide')) {
+
+      checkboxForest.checked = false;
+      checkboxRain.checked = false;
+      checkboxCoffeeShop.checked = false;
+      checkboxFireplace.checked = false;
+      rainSound.pause();
+      coffeeShopSound.pause();
+      fireplaceSound.pause();
+      forestSound.pause();
+
+    }
+
+  }
+
+  function changeVolume(id, value) {
+
+    if (id == 'forestVolume') {
+      forestSound.volume = value / 10;
+      console.log(forestSound)
+      console.log(forestSound.volume)
+    }
+
+    if (id == 'rainVolume') {
+      rainSound.volume = value / 10;
+      console.log()
+    }
+
+    if (id == 'coffeeShopVolume') {
+      coffeeShopSound.volume = value / 10;
+      console.log()
+    }
+
+    if (id == 'fireplaceVolume') {
+      fireplaceSound.volume = value / 10;
+      console.log()
+    }
+
+    // forestSound.volume = 1;
+    // rainSound.volume = 1;
+    // coffeeShopSound.volume = 1;
+    // fireplaceSound.volume = 1;
+
+  }
+
+  return {
+    onOffForestSound,
+    onOffRainSound,
+    onOffCoffeeShopSound,
+    onOffFireplaceSound,
+    finishSound,
+    pauseAllSounds,
+    changeVolume
+  }
+}
